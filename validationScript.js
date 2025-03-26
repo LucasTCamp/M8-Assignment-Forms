83ae20934d2c1db04a179bec3c2141461a723f25
YOUR_NAME <YOUR_EMAIL>
2025-03-26 13:14:13 -0400

    Finsihed

--- validationScript.js
+++ validationScript.js
@@ -1,23 +1,18 @@
     // JavaScript code for form validation
-	// Prevent form from submitting
+
 document.getElementById("myForm").addEventListener("submit", function(event) {
+  // Prevent form from submitting
+    event.preventDefault();
   // Retrieve the input field value
-  const inputField = document.getElementById("inputField");
+    const inputField = document.getElementById("inputField");
  // Regular expression pattern for alphanumeric input
-  const regex = /^[a-zA-Z0-9_]*$,/;
+    const regex = /^[a-zA-Z0-9]+$/;
  // Check if the input value matches the pattern
  // Valid input: display confirmation and submit the form
-  if (regex.test(inputField)) {
-      const valid = document.createElement("p");
-      valid.textContent = "VALID";
-      document.getElementById("myForm").appendChild(valid);
-// Invalid input: display error message
-  } else {
-    event.preventDefault();
-    const invalid = document.createElement("p");
-    invalid.textContent = "INVALID";
-    document.getElementById("myForm").appendChild(invalid);
-  }
-   
-
+    if (regex.test(inputField.value)) {
+        alert("VALID");
+ // Invalid input: display error message
+    } else {
+        alert("INVALID");
+    }
 });
